9dfb69696db658a5f76298a1472110a1d57170ae
Karen <K.Blanchette07@gmail.com>
2017-09-30 20:23:59 -0500

    HTTPS correction

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -24,7 +24,7 @@ function listGIFs(){
 	$('#img-container').html('');
 	var imageTitle = $(this).attr("data-topic");
 	var limit = 10;
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + imageTitle + "&api_key=dc6zaTOxFJmzC&limit=" + limit + "&rating=g";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + imageTitle + "&api_key=dc6zaTOxFJmzC&limit=" + limit + "&rating=g";
 	$.ajax({
 		url: queryURL,
 		method: "GET"			
